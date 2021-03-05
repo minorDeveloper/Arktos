2de98b25fc40f1d56acbec9fd00253c726fea16e
theMinorDeveloper <theminordeveloper@outlook.com>
2021-03-05 16:44:35 +0000

    ImGui Cmake additions

--- Arktos/src/app.cpp
+++ Arktos/src/app.cpp
@@ -8,46 +8,47 @@
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Shaders/VertexColor.h>
 
-#include <stdio.h>
+#include <iostream>
+#include <math.h>
 #include "Arktos/Log.h"
+
 
 using namespace Magnum;
 
-class BaseApplication: public Platform::Application {
-    public:
-        explicit BaseApplication(const Arguments& arguments);
+class BaseApplication : public Platform::Application {
+public:
+    explicit BaseApplication(const Arguments &arguments);
 
-    private:
-        void drawEvent() override;
+private:
+    void drawEvent() override;
 
-        GL::Mesh _mesh;
-        Shaders::VertexColor2D _shader;
+    GL::Mesh _mesh;
+    Shaders::VertexColor2D _shader;
 };
 
-BaseApplication::BaseApplication(const Arguments& arguments):
-    Platform::Application{ arguments, Configuration{}.setTitle("Magnum Triangle Example") }
-{
+BaseApplication::BaseApplication(const Arguments &arguments) :
+        Platform::Application{arguments, Configuration{}.setTitle("Magnum Triangle Example")} {
     using namespace Math::Literals;
     Arktos::Log::Init();
-    Arktos::Log::GetCoreLogger()->warn("Initilised log!");
-    Arktos::Log::GetClientLogger()->info("Initilised log!");
+    Arktos::Log::GetCoreLogger()->warn("Initialised log!");
+    Arktos::Log::GetClientLogger()->info("Initialised log!");
     struct TriangleVertex {
         Vector2 position;
         Color3 color;
     };
     const TriangleVertex data[]{
-        {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
-        {{ 0.5f, -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
-        {{ 0.0f,  0.5f}, 0x0000ff_rgbf}     /* Top vertex, blue color */
+            {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
+            {{0.5f,  -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
+            {{0.0f,  0.5f},  0x0000ff_rgbf}     /* Top vertex, blue color */
     };
 
     GL::Buffer buffer;
     buffer.setData(data);
 
     _mesh.setCount(3)
-        .addVertexBuffer(std::move(buffer), 0,
-            Shaders::VertexColor2D::Position{},
-            Shaders::VertexColor2D::Color3{});
+            .addVertexBuffer(std::move(buffer), 0,
+                             Shaders::VertexColor2D::Position{},
+                             Shaders::VertexColor2D::Color3{});
 }
 
 void BaseApplication::drawEvent() {
