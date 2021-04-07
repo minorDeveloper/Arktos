649d619a47ef1f612bd6e0d1a27da0369cc6b752
theMinorDeveloper <theminordeveloper@outlook.com>
2021-04-07 11:40:37 +0100

    Merged

--- Eos/apps/main.cu
+++ Eos/apps/main.cu
@@ -1,28 +1,7 @@
-#include "Eos/particle.cuh"
-#include <stdlib.h>
-#include <stdio.h>
 
-__global__
-void advanceParticles(float dt, particle * pArray, int nParticles)
-{
-    int idx = threadIdx.x + blockIdx.x*blockDim.x;
-    if(idx < nParticles) { pArray[idx].advance(dt); }
-}
 
+#include "Eos.cuh"
 
 int main(int argc, char ** argv) {
-    int n = 1000000;
-    if (argc > 1) { n = atoi(argv[1]); }     // Number of particles
-    if (argc > 2) { srand(atoi(argv[2])); } // Random seed
-
-    particle *pArray = new particle[n];
-    particle *devPArray = NULL;
-    cudaMalloc(&devPArray, n * sizeof(particle));
-    cudaMemcpy(devPArray, pArray, n * sizeof(particle), cudaMemcpyHostToDevice);
-    for (int i = 0; i < 100; i++) {   // Random distance each step
-        float dt = (float) rand() / (float) RAND_MAX;
-        advanceParticles<<< 1 + n / 256, 256>>>(dt, devPArray, n);
-        cudaDeviceSynchronize();
-    }
-    return 0;
+    return run(argc, argv);
 }
